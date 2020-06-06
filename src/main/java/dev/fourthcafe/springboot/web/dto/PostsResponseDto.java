4a840c641410a3286be858c0a5f291070600de75
fourthcafe <fourthcafe@gmail.com>
2020-06-07 03:03:52 +0900

    3.4 등록/수정/조회 API 만들기

---
+++ src/main/java/dev/fourthcafe/springboot/web/dto/PostsResponseDto.java
@@ -0,0 +1,19 @@
+package dev.fourthcafe.springboot.web.dto;
+
+import dev.fourthcafe.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+	private Long id;
+	private String title;
+	private String content;
+	private String author;
+
+	public PostsResponseDto(Posts entity) {
+		this.id = entity.getId();
+		this.title = entity.getTitle();
+		this.content = entity.getContent();
+		this.author = entity.getAuthor();
+	}
+}
