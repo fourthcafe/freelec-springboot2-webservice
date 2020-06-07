a0d4d0d945f030d2a9ad06bfed17c68eed207f84
fourthcafe <fourthcafe@gmail.com>
2020-06-08 00:28:45 +0900

    5.4 어노테이션 기반으로 개선하기

--- src/main/java/dev/fourthcafe/springboot/web/IndexController.java
+++ src/main/java/dev/fourthcafe/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package dev.fourthcafe.springboot.web;
 
+import dev.fourthcafe.springboot.config.auth.LoginUser;
 import dev.fourthcafe.springboot.config.auth.dto.SessionUser;
 import dev.fourthcafe.springboot.service.PostsService;
 import dev.fourthcafe.springboot.web.dto.PostsResponseDto;
@@ -9,20 +10,16 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import javax.servlet.http.HttpSession;
-
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 	private final PostsService postsService;
-	private final HttpSession httpSession;
 
 
 	@GetMapping("/")
-	public String index(Model model) {
+	public String index(Model model, @LoginUser SessionUser user) {
 		model.addAttribute("posts", postsService.findAllDesc());
 
-		final SessionUser user = (SessionUser) httpSession.getAttribute("user");
 		if (user != null) {
 			model.addAttribute("userName", user.getName());
 		}
